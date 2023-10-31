ff0c7876354253e3e4298b4d24eebe72989f4461
Daniel Dimitrov <unwill35@gmail.com>
2023-11-01 01:52:51 +0200

    Update omni_tb8321p2_bsp.mk

--- omni_tb8321p2_bsp.mk
+++ omni_tb8321p2_bsp.mk
@@ -1,5 +1,5 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_minimal.mk
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_minimal.mk)
 
 # Another common config inclusion
 $(call inherit-product, $(SRC_TARGET_DIR)/product/embedded.mk)
